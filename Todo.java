3a0d2f14b09cc06c4988832975c260ac2e35c7e6
Brett Arnold <Brett.M.Arnold-1@ou.edu>
2019-02-12 21:19:13 -0600

    Commit todo

---
+++ Todo.java
@@ -0,0 +1,28 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-12
+OU ID_______________: 113421057
+OU EMAIL____________: Brett.M.Arnold-1@ou.edu
+
+Tasks:
+1. Create Animal Tests 
+2. Create Equipment Tests
+3. Create Zoo Tests 
+4. Ensure all tests are valid
+5. Push Classes into git
+
+Estimated Time (minutes):
+1. 20
+2. 20
+3. 20
+4. 20
+5. 10
+
+Actual Time (minutes):
+1. 20
+2. 30
+3. 30
+4. 10
+5. 10
+
+*/}
